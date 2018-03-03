88e70cb4f84d38101a705505886eb665ae6d03f4
Behrooz Babazadeh <Behrooz.Babazadeh09@gmail.com>
2018-03-03 12:26:04 -0500

    React-Redux 5 day weather app

---
+++ WeatherApp/src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+
+const API_KEY = 'c3f0397ce8b3ee0ffabee9fcd36557ca';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us&units=imperial`;
+  const request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
